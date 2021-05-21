a0930d5f50d37b736ae73de8985ff06fe06f1195
Kirkados <khovell@gmail.com>
2021-05-21 17:39:00 -0400

    5Hz serverRate

--- Projects/Kirk_Phase3/check_guidance.m
+++ Projects/Kirk_Phase3/check_guidance.m
@@ -1,21 +1,49 @@
 % This script analyzes expeirmental results
 
+close all
 
 figure()
 plot(rt_dataPacket(:,1),rt_dataPacket(:,65))
 hold on;
-plot(rt_dataPacket(:,1),rt_dataPacket(:,68))
+plot(rt_dataPacket(:,1),rt_dataPacket(:,71))
 legend('Accel X command', 'True Ax')
 
 
 figure()
 plot(rt_dataPacket(:,1),rt_dataPacket(:,66))
 hold on;
-plot(rt_dataPacket(:,1),rt_dataPacket(:,69))
+plot(rt_dataPacket(:,1),rt_dataPacket(:,72))
 legend('Accel Y command', 'True Ay')
 
 figure()
 plot(rt_dataPacket(:,1),rt_dataPacket(:,67))
 hold on;
-plot(rt_dataPacket(:,1),rt_dataPacket(:,70))
+plot(rt_dataPacket(:,1),rt_dataPacket(:,73))
 legend('Alpha command', 'True Alpha')
+
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,68))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,81))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,78))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,75))
+legend('Shoulder  \alpha command', 'Shoulder \omega command', 'True shoulder \omega','\theta shoulder')
+
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,69))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,82))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,79))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,76))
+legend('Elbow  \alpha command', 'Elbow \omega command', 'True Elbow \omega','\theta elbow')
+
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,70))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,83))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,80))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,77))
+legend('Wrist  \alpha command', 'Wrist \omega command', 'True wrist \omega','\theta wrist')
