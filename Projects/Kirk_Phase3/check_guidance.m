5eaf09f7d1917a267245f03945d794917ea0aae7
Kirkados <khovell@gmail.com>
2021-06-29 09:57:41 -0400

    plotting joints

--- Projects/Kirk_Phase3/check_guidance.m
+++ Projects/Kirk_Phase3/check_guidance.m
@@ -24,32 +24,32 @@ hold on;
 plot(rt_dataPacket(:,1),rt_dataPacket(:,73))
 legend('Alpha command', 'True Alpha')
 
-% figure()
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,68))
-% hold on;
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,81))  
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,78))
-% yyaxis right
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,75))
-% legend('Shoulder  \alpha command', 'Shoulder \omega command', 'True shoulder \omega','\theta shoulder')
-% 
-% figure()
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,69))
-% hold on;
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,82))  
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,79))
-% yyaxis right
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,76))
-% legend('Elbow  \alpha command', 'Elbow \omega command', 'True Elbow \omega','\theta elbow')
-% 
-% figure()
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,70))
-% hold on;
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,83))  
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,80))
-% yyaxis right
-% plot(rt_dataPacket(:,1),rt_dataPacket(:,77))
-% legend('Wrist  \alpha command', 'Wrist \omega command', 'True wrist \omega','\theta wrist')
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,68))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,81))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,78))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,75))
+legend('Shoulder  \alpha command', 'Shoulder \omega command', 'True shoulder \omega','\theta shoulder')
+
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,69))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,82))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,79))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,76))
+legend('Elbow  \alpha command', 'Elbow \omega command', 'True Elbow \omega','\theta elbow')
+
+figure()
+plot(rt_dataPacket(:,1),rt_dataPacket(:,70))
+hold on;
+plot(rt_dataPacket(:,1),rt_dataPacket(:,83))  
+plot(rt_dataPacket(:,1),rt_dataPacket(:,80))
+yyaxis right
+plot(rt_dataPacket(:,1),rt_dataPacket(:,77))
+legend('Wrist  \alpha command', 'Wrist \omega command', 'True wrist \omega','\theta wrist')
 
 figure()
 plot(rt_dataPacket(:,1),rt_dataPacket(:,85))
